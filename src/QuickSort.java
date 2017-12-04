71090e671c8f42780d6eb5e03c1e773597c23296
lxwu <lxwu@travelsky.com>
2017-12-04 22:02:16 +0800

    快速排序算法和字符串反转

---
+++ src/QuickSort.java
@@ -0,0 +1,48 @@
+import javax.xml.stream.events.StartDocument;
+
+
+public class QuickSort {
+	public void quickSort(int a[],int start, int end){
+		int low = start;
+		int high = end;
+		int key = a[start];
+		while (high > low) {			
+			while (a[high] >= key && high > low) {
+				high--;
+			}
+			if(a[high] <= key && high > low){
+				swap(a, low, high); 
+			}
+			while(a[low] <= key && low < high){
+				low++;
+			}
+			if(a[low] >= key && high > low){
+				swap(a, low, high); 
+			}
+			if(low > start){
+				quickSort(a, start, low-1);
+			}
+			if(high < end){
+				quickSort(a, high+1, end);
+			}
+		}
+		}
+
+
+	private void swap(int[] a, int low, int high) {
+		int temp = a[high];
+		a[high] = a[low];
+		a[low] = temp;
+	}
+		
+	
+	public static void main(String[] args) {
+		int[] a = {5, 3, 1, 22,8,7,2};
+		QuickSort ddQuickSort = new QuickSort();
+		ddQuickSort.quickSort(a, 0, a.length-1);
+		for (int b = 0; b < a.length; b++) {
+			System.out.println(a[b]);
+		}
+		}
+	}
+
---
+++ src/StringReverse.java
@@ -0,0 +1,87 @@
+
+public class StringReverse {
+	public String reverseS(String targetString){
+		if(null != targetString && targetString.length() > 1){
+				return reverseS(targetString.substring(1))+targetString.charAt(0);
+		}
+		return targetString;
+	}
+	
+	public int string2Int(String str){
+		if(null != str && str.length() >0){
+			int sumInt = 0;
+			for(int i = 0; i<str.length();i++){
+				sumInt = sumInt * 10 + (str.charAt(i)-'0' + 0);
+			}
+			return sumInt;
+		}
+		return -1;
+	}
+
+	public String int2String(int num){		
+		char[] temp = new char[7] ;
+		StringBuffer afterString = new StringBuffer(); 
+		int i = 0;
+		while(num > 0){
+			temp[i] = (char) (num % 10 + '0');
+			num = num / 10;
+			i++;
+		}
+		System.out.println("temp is" + temp);
+		//reverse the number
+		for (int j = i; j >= 0; j--) {
+			afterString.append(temp[j]);
+		}		
+		return afterString.toString();
+	}
+	
+	public String loopMove(String originalString, int step){
+		if (null != originalString && originalString.length() > step && step > 1 ) {
+			int index = originalString.length()-step;
+			String moveString = originalString.substring(index);
+			return moveString + originalString.substring(0, index);
+		}
+		return null;
+	}
+	
+	public String longestRepeatStr(String str){
+			    int n = str.length();
+			    for(int i = n - 1; i > 1; --i)
+			         for(int j = 0; j < n; ++j) {
+			             if(i + j <= n) {
+			                String cur = str.substring(j,i);
+			                int index1 = str.indexOf(cur); // 从前往后找
+			                int index2 = str.lastIndexOf(cur); // 从后往前找
+			                if(index1 != index2)
+			                    return cur;
+			            }
+			        }
+			    return null;
+	}
+	
+	
+	public static void main(String[] args) {
+		String originalString = "abcdefgh";
+		StringReverse stringReverse = new StringReverse();
+		Integer integer = new Integer(0);
+		String afterReverseString = stringReverse.reverseS(originalString);
+		System.out.println("the originalString is " + originalString);
+		System.out.println("the reverseString is " + afterReverseString);
+		
+		int aaa = -1;
+		String int2String = stringReverse.int2String(aaa);
+		System.out.println("before is " + aaa);
+		System.out.println("afterString is " + int2String);
+		
+		String bbb = "986";
+		int numInt = stringReverse.string2Int(bbb);
+		System.out.println("numInt  is " +numInt);
+		String loopString = stringReverse.loopMove(originalString, 3);
+		System.out.println("loopString is " + loopString);
+		
+		String longestRepeatStr = stringReverse.longestRepeatStr(originalString);
+		System.out.println("longestRepeatStr is " + longestRepeatStr);
+	}
+	
+	 
+}
